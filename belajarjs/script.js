d7743821db4adff28a44b6fd62ce6744f3c65c10
ArbAlviano <arbialviano@gmail.com>
2026-08-02 07:59:04 +0700

    belajar js

---
+++ belajarjs/script.js
@@ -0,0 +1,13 @@
+/*function cekGanjilGenap (number){
+    if (number % 2 === 0){
+        return "genap";
+    } else {
+        return "ganjil";
+    }
+}
+
+console.log(cekGanjilGenap(10));
+console.log(cekGanjilGenap(7));*/
+
+let ganti = document.getElementById("manipulasi");
+ganti.textContent = "WORLD HELLO";
